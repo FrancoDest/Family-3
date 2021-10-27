06212a505f0bc652cf32f7200a53cb17b06cab7c
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2021-10-27 18:51:23 -0300

    Cambio nombres de los miembros de la familia y agrego comentario

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,15 +7,15 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            //Perdon por la originalidad en los nombres, si tengo hijos espero que mi pareja tenga buena creatividad (chiste para romper el hielo)
-            Node n1 = new Node("Raul",70);
-            Node n2 = new Node("Raula",40);
-            Node n3 = new Node("Rauliño",38);
-            Node n4 = new Node("Rauliña",31);
-            Node n5 = new Node("Raula Jr",20);
-            Node n6 = new Node ("Rauliño Jr",19);
-            Node n7 = new Node("Rauliña Jr",10);
-            Node n8 = new Node("Rauliña Jr Jr",1);
+            //Deje que el arbol familiar sea con 1 padre solo para cada hijo, ya que no queria modificar mas de lo necesario la clase Node
+            Node n1 = new Node("Maria",70);
+            Node n2 = new Node("Mario",40);
+            Node n3 = new Node("Mariana",38);
+            Node n4 = new Node("Mariano",31);
+            Node n5 = new Node("Mario Jr",20);
+            Node n6 = new Node ("Mariano Jr",19);
+            Node n7 = new Node("Mariana Jr",10);
+            Node n8 = new Node("Mario Jr Jr",1);
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
@@ -27,9 +27,9 @@ namespace Program
 
             n7.AddChildren(n8);
 
-            VisitorAgeCalculator sumadeedades = new VisitorAgeCalculator();
-            n1.AcceptVisitor(sumadeedades);
-            Printer.print(sumadeedades);
+            VisitorAgeCalculator Sumadeedades = new VisitorAgeCalculator();
+            n1.AcceptVisitor(Sumadeedades);
+            Printer.print(Sumadeedades);
 
             VisitorOlderchild Hijomayor = new VisitorOlderchild();
             n1.AcceptVisitor(Hijomayor);
